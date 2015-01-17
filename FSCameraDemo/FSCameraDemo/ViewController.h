1f3bba03b13c568580a4cee9d0c2abddcc0180ab
ForryShih <374784680@qq.com>
2015-01-17 10:19:59 +0800

    Created the project.

---
+++ FSCameraDemo/FSCameraDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FSCameraDemo
+//
+//  Created by ForryShih on 1/17/15.
+//  Copyright (c) 2015 Rampage Works. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
